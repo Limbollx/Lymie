c61f0f4a93838da534999a1f7232c9dd2a6cf37a
Limbollx <Ultimaboll@gmail.com>
2022-10-31 08:03:16 +0400

    1st Version

---
+++ src/main/java/net/limboll/lymie/Lymie.java
@@ -0,0 +1,30 @@
+package net.limboll.lymie;
+
+import com.mojang.logging.LogUtils;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.slf4j.Logger;
+
+@Mod(Lymie.MOD_ID)
+public class Lymie
+{
+    public static final String MOD_ID = "lymie";
+    private static final Logger LOGGER = LogUtils.getLogger();
+
+    public Lymie()
+    {
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event)
+    {
+        // some preinit code
+        LOGGER.info("HELLO FROM PREINIT");
+        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    }
+}
