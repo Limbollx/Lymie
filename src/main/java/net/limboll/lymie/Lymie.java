b245c62c43c0991049d68e14f65c5972ca9a01fe
Limbollx <Ultimaboll@gmail.com>
2022-11-08 18:51:52 +0400

    Added First Item and more Directory

--- src/main/java/net/limboll/lymie/Lymie.java
+++ src/main/java/net/limboll/lymie/Lymie.java
@@ -1,8 +1,10 @@
 package net.limboll.lymie;
 
 import com.mojang.logging.LogUtils;
+import net.limboll.lymie.item.ModItems;
 import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -16,15 +18,16 @@ public class Lymie
 
     public Lymie()
     {
-        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        IEventBus eventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        ModItems.register(eventBus);
+
+        eventBus.addListener(this::setup);
 
         MinecraftForge.EVENT_BUS.register(this);
     }
 
     private void setup(final FMLCommonSetupEvent event)
     {
-        // some preinit code
-        LOGGER.info("HELLO FROM PREINIT");
-        LOGGER.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 }
--- src/main/java/net/limboll/lymie/item/ModItems.java
+++ src/main/java/net/limboll/lymie/item/ModItems.java
@@ -1,4 +1,21 @@
 package net.limboll.lymie.item;
 
+import net.limboll.lymie.Lymie;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
 public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, Lymie.MOD_ID);
+
+    public static final RegistryObject<Item> SILVER_INGOTS = ITEMS.register("silver-ingots",
+            () -> new Item(new Item.Properties().tab(CreativeModeTab.TAB_MATERIALS)));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
 }
